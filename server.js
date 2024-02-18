7518c960d1a3a5a850f0eaf9401fe90f5d7d491b
hewseo <hewseo03@gmail.com>
2024-02-19 01:46:45 +0900

    수정

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const http = require('http');
-let ejs = require('ejs');
 const PORT = 3000
 
 app = express()
 server = http.createServer(app);
 
-app.get('/login.html', function (req, res) {
+app.get('/login', function (req, res) {
   res.send("로그인 화면에 도달했습니다")
 });
 
